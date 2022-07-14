cde131fb61d9ffaacdd86677cb44ae80f61faf6a
Andres Saenz <asaenzcordero@gmail.com>
2022-07-14 18:35:06 -0500

    solver

--- src/main/java/challenge/ReactiveExample.java
+++ src/main/java/challenge/ReactiveExample.java
@@ -4,7 +4,12 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 
 public class ReactiveExample {
@@ -14,20 +19,28 @@ public class ReactiveExample {
 
     public ReactiveExample() {
         //TODO: convertir los estudiantes a un Flux
-
-       /* estudianteList = List.of(
+        estudianteList = Flux.just(
                 new Estudiante("raul", 30, List.of(1, 2, 1, 4, 5)),
                 new Estudiante("andres", 35, List.of(4, 2, 4, 3, 5)),
                 new Estudiante("juan", 75, List.of(3, 2, 4, 5, 5)),
                 new Estudiante("pedro", 80, List.of(5, 5, 4, 5, 5)),
                 new Estudiante("santiago", 40, List.of(4, 5, 4, 5, 5))
         );
-*/
+
+    }
+
+    private static Integer apply(Estudiante estudiante) {
+        return estudiante.getAsistencias().size();
+    }
+
+    private static Estudiante apply2(Estudiante estudiante) {
+        return estudiante;
     }
 
     //TODO: suma de puntajes
     public Mono<Integer> sumaDePuntajes() {
-        return null;
+       return estudianteList.map(Estudiante::getPuntaje)
+                .reduce(Integer::sum);
     }
 
     private Function<Estudiante, Integer> mapeoDeEstudianteAPuntaje() {
@@ -35,37 +48,67 @@ public class ReactiveExample {
     }
 
     //TODO: mayor puntaje de estudiante
-    public Mono<Estudiante> mayorPuntajeDeEstudiante(int limit) {
-        return null;
+    public Mono<Estudiante> mayorPuntajeDeEstudiante() {
+        return  estudianteList.sort(Comparator.reverseOrder())
+                .map(ReactiveExample::apply2).elementAt(0);
 
     }
 
     //TODO: total de asisntencias de estudiantes con mayor puntaje basado en un  valor
     public Mono<Integer> totalDeAsisntenciasDeEstudiantesConMayorPuntajeDe(int valor) {
-        return null;
+        return estudianteList.filter(estudiante -> {
+                    return estudiante.getPuntaje() >= valor;
+                })
+                .map(ReactiveExample::apply)
+                .reduce(Integer::sum);
     }
 
     //TODO: el estudiante tiene asistencias correctas
     public Mono<Boolean> elEstudianteTieneAsistenciasCorrectas(Estudiante estudiante) {
-        return null;
+        Mono<Estudiante> est = Mono.just(estudiante);
+        est.filter(asistenciasPemitidas());
+        return est
+               .hasElement();
+
     }
 
     //TODO: promedio de puntajes por estudiantes
     public Mono<Double> promedioDePuntajesPorEstudiantes() {
-        return null;
+        return estudianteList.collect(Collectors.averagingDouble(Estudiante::getPuntaje));
     }
 
 
     //TODO: los nombres de estudiante con puntaje mayor a un valor
     public Flux<String> losNombresDeEstudianteConPuntajeMayorA(int valor) {
-        return null;
+        return estudianteList.filter(estudiante -> estudiante.getPuntaje() > valor)
+                .map(Estudiante::getNombre);
     }
 
 
 
     //TODO: estudiantes aprovados
     public Flux<String> estudiantesAprovados(){
-        return null;
+        Flux<String> estudiantes = estudianteList.map(this::aprobar)
+                .filter(Estudiante::isAprobado)
+                .map(Estudiante::getNombre);
+        return estudiantes;
+    }
+
+
+    private Estudiante aprobar(Estudiante estudiante) {
+        return Optional.of(estudiante)
+                .filter(e -> e.getPuntaje() >= 75)
+                .map(e -> {
+                    var est1 = new Estudiante(e.getNombre(), e.getPuntaje(), e.getAsistencias());
+                    est1.setAprobado(true);
+                    return est1;
+                }).orElseGet(() -> estudiante);
+    }
+
+    private Predicate<Estudiante> asistenciasPemitidas() {
+        return estudiante -> estudiante.getAsistencias()
+                .stream()
+                .reduce(0, Integer::sum) >= VALOR_PERMITIDO;
     }
 
 
--- src/test/java/challenge/ReactiveExampleTest.java
+++ src/test/java/challenge/ReactiveExampleTest.java
@@ -1,9 +1,13 @@
 package challenge;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.test.StepVerifier;
+
+import java.util.List;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -15,31 +19,61 @@ class ReactiveExampleTest {
 
     @Test
     void sumaDePuntajes(){
+           var suma = reactiveExample.sumaDePuntajes();
+        StepVerifier.create(suma)
+                .expectNext(260)
+                .verifyComplete();
+    }
 
+    @Test
+    void losNombresDeEstudianteConPuntajeMayorA() {
+        var valor = reactiveExample.losNombresDeEstudianteConPuntajeMayorA(75);
+        StepVerifier.create(valor)
+                .expectNext("pedro")
+                .verifyComplete();
     }
 
     @Test
     void mayorPuntajeDeEstudiante(){
-
+        var mayor = reactiveExample.mayorPuntajeDeEstudiante();
+        StepVerifier.create(mayor)
+                .expectNextMatches(response -> response.getPuntaje().equals(80))
+                .verifyComplete();
     }
 
     @Test
     void totalDeAsisntenciasDeEstudiantesComMayorPuntajeDe(){
-
+        var mayor = reactiveExample.totalDeAsisntenciasDeEstudiantesConMayorPuntajeDe(75);
+        StepVerifier.create(mayor)
+                .expectNext(10)
+                .verifyComplete();
     }
 
     @Test
     void elEstudianteTieneAsistenciasCorrectas(){
+        var estudiante = new Estudiante("pedro", 80, List.of(5, 5, 4, 5, 5));
+        var asistenciasCorrectas = reactiveExample.elEstudianteTieneAsistenciasCorrectas(estudiante);
 
+        StepVerifier.create(asistenciasCorrectas)
+                .expectNext(true)
+                .verifyComplete();
     }
 
     @Test
     void promedioDePuntajesPorEstudiantes(){
-
+        var promedio = reactiveExample.promedioDePuntajesPorEstudiantes();
+        StepVerifier.create(promedio)
+                .expectNext(52.0)
+                .verifyComplete();
     }
 
     @Test
     void estudiantesAprovados(){
-
+        var aprovados = reactiveExample.estudiantesAprovados();
+        StepVerifier.create(aprovados)
+                .expectNext("juan", "pedro")
+                .verifyComplete();
     }
+
+
 }
